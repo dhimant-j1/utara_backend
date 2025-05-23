e53664e0b4f55aeb9fb3b4d44ee34499c9d85eeb
bharatmk256 <bharatmk257@gmail.com>
2025-05-23 21:07:56 +0530

    added cors issues

--- routes/routes.go
+++ routes/routes.go
@@ -15,7 +15,7 @@ func SetupRoutes(r *gin.Engine) {
 
 	// CORS config
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:50855"},
+		AllowOrigins:     []string{"https://utara-app.web.app"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
